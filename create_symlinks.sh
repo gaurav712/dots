82fe3221ab0abb6829693a77ff49278de0efec24
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-03-24 17:35:24 +0530

    add neovim to symlinks list

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -10,6 +10,7 @@ links[".xinitrc"]="$HOME/.xinitrc"
 links[".bashrc"]="$HOME/.bashrc"
 links[".config/alacritty"]="$HOME/.config/alacritty"
 links[".config/dunst"]="$HOME/.config/dunst"
+links[".config/nvim"]="$HOME/.config/nvim"
 
 for key in "${!links[@]}"; do
   echo "ln -sf" "$(pwd)/$key" "${links[$key]}"
