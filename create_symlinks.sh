3d85bd504aa9d2d3017937e15be5579291946d2c
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-03-29 15:17:55 +0530

    unify the two scripts & implement an OS check

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -3,16 +3,36 @@
 # create the dictionary
 declare -A links
 
+# Check OS
+# 
+# Credits to https://stackoverflow.com/users/14860/paxdiablo for this snippet
+
+unameOut="$(uname -s)"
+
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
 # Define the symlinks sources and targets
 
-links[".tmux.conf"]="$HOME/.tmux.conf"
-links[".xinitrc"]="$HOME/.xinitrc"
-links[".bashrc"]="$HOME/.bashrc"
-links[".config/alacritty"]="$HOME/.config/alacritty"
-links[".config/dunst"]="$HOME/.config/dunst"
 links[".config/nvim"]="$HOME/.config/nvim"
-links[".config/picom.conf"]="$HOME/.config/picom.conf"
-links["bg.jpg"]="$HOME/.config/bg"
+
+if [ $machine == "Linux" ]; then
+  links[".tmux.conf"]="$HOME/.tmux.conf"
+  links[".xinitrc"]="$HOME/.xinitrc"
+  links[".bashrc"]="$HOME/.bashrc"
+  links[".config/alacritty"]="$HOME/.config/alacritty"
+  links[".config/dunst"]="$HOME/.config/dunst"
+  links[".config/picom.conf"]="$HOME/.config/picom.conf"
+  links["bg.jpg"]="$HOME/.config/bg"
+elif [ $machine == "Mac" ]; then
+  links["mac/.tmux.conf"]="$HOME/.tmux.conf"
+  links["mac/.config/alacritty"]="$HOME/.config/alacritty"
+  links["bg.jpg"]="$HOME/Pictures/bg.jpg"
+fi
+
 
 for key in "${!links[@]}"; do
 
