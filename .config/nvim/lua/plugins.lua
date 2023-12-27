db3d878fabd548a0f54f2061c03ce2ed752a3b9a
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-12-28 02:37:43 +0530

    change version number for telescope

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -25,7 +25,7 @@ return require('packer').startup(function(use)
 
   -- Fuzzy finder
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
