6c898412c7e9d06b43e2237c8f0473bc11f1523f
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2025-01-28 15:10:42 +0530

    add information on configs

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -1,58 +1,59 @@
 local g = vim.g
 local o = vim.o
 
+-- Enable 24-bit RGB color in the terminal (for better colors)
 o.termguicolors = true
 
--- Map <leader> to ;
+-- Map the <leader> key to ';' (commonly used as a prefix for custom shortcuts)
 g.mapleader = ';'
 g.maplocalleader = ';'
 
--- Do not save when switching buffers
+-- Allow switching between buffers without saving the current buffer
 o.hidden = true
 
--- Better editor UI
-o.number = true
-o.numberwidth = 5
-o.relativenumber = true
-o.signcolumn = 'yes:2'
-o.cursorline = true
-o.colorcolumn = '80'
+-- Editor UI options
+o.number = true           -- Show line numbers
+o.numberwidth = 5         -- Set the width of the number column to 5
+o.relativenumber = true   -- Show relative line numbers (useful for navigation)
+o.signcolumn = 'yes:2'    -- Always show a 2-column wide sign column (for git, diagnostics, etc.)
+o.cursorline = true       -- Highlight the current line
+o.colorcolumn = '80'      -- Highlight column 80 (useful for line length limits)
 
--- Better editing experience
-o.expandtab = true
--- o.smarttab = true
-o.cindent = true
--- o.autoindent = true
-o.wrap = true
-o.textwidth = 300
-o.tabstop = 2
-o.shiftwidth = 0
-o.softtabstop = -1 -- If negative, shiftwidth value is used
-o.list = true
-o.listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂'
+-- Editing experience options
+o.expandtab = true        -- Use spaces instead of tabs
+-- o.smarttab = true       -- (Commented out) Insert spaces when pressing <Tab> in insert mode
+o.cindent = true          -- Enable C-style indentation
+-- o.autoindent = true     -- (Commented out) Automatically indent new lines
+o.wrap = true             -- Wrap long lines
+o.textwidth = 300         -- Maximum width for text wrapping (set very high)
+o.tabstop = 2             -- Number of spaces for a tab character
+o.shiftwidth = 0          -- Use the value of 'tabstop' for auto-indentation
+o.softtabstop = -1        -- If negative, use the value of 'shiftwidth' for <Tab>/<BS>
+o.list = true             -- Show invisible characters
+o.listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂' -- Define how invisible characters are displayed
 
--- Enable mouse
+-- Enable mouse support in all modes
 o.mouse = 'a'
 
--- Makes neovim and host OS clipboard play nicely with each other
+-- Synchronize Neovim clipboard with the system clipboard
 o.clipboard = 'unnamedplus'
 
--- Case insensitive searching UNLESS /C or capital in search
+-- Make searching case-insensitive unless a capital letter is used or /C is specified
 o.ignorecase = true
 o.smartcase = true
 
 -- Undo and backup options
-o.backup = false
-o.writebackup = false
-o.undofile = false
-o.swapfile = false
+o.backup = false          -- Do not create backup files
+o.writebackup = false     -- Do not keep a backup while overwriting a file
+o.undofile = false        -- Do not save undo history to a file
+o.swapfile = false        -- Disable swap files
 
--- Remember 50 items in commandline history
+-- Set the command-line history to remember the last 50 commands
 o.history = 50
 
--- Better buffer splitting
+-- Split windows to the right and below by default
 o.splitright = true
 o.splitbelow = true
 
--- Improve UI update times
+-- Reduce the time Neovim waits for a change (in milliseconds)
 o.updatetime = 300
