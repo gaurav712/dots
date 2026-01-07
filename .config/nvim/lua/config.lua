708e16131c1bef114aa3317b4e68cb1b1021d462
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2026-01-08 04:47:04 +0530

    refactor and simplify config

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -1,20 +1,16 @@
 local g = vim.g
 local o = vim.o
 
-o.fileencoding = 'utf-8'        -- Use UTF-8 encoding for files
-o.fileformats = 'unix,dos,mac'  -- Automatically detect line endings
-
--- Enable 24-bit RGB color in the terminal (for better colors)
-o.termguicolors = true
-
--- Map the <leader> key to ';' (commonly used as a prefix for custom shortcuts)
 g.mapleader = ' '
-g.maplocalleader = ';'
 
--- Allow switching between buffers without saving the current buffer
-o.hidden = true
+-- Editor
+o.fileencoding = 'utf-8'        -- Use UTF-8 encoding for files
+o.hidden = true           -- Allow switching between buffers without saving the current buffer
+o.expandtab = true        -- Use spaces instead of tabs
+o.cindent = true          -- Enable C-style indentation
 
--- Editor UI options
+-- UI
+o.termguicolors = true    -- Enable 24-bit RGB colors
 o.number = true           -- Show line numbers
 o.numberwidth = 5         -- Set the width of the number column to 5
 o.relativenumber = true   -- Show relative line numbers (useful for navigation)
@@ -23,19 +19,14 @@ o.cursorline = true       -- Highlight the current line
 o.colorcolumn = '80'      -- Highlight column 80 (useful for line length limits)
 o.hlsearch = true         -- Highlight search matches
 o.incsearch = true        -- Show matches as you type
-
--- Editing experience options
-o.expandtab = true        -- Use spaces instead of tabs
--- o.smarttab = true       -- (Commented out) Insert spaces when pressing <Tab> in insert mode
-o.cindent = true          -- Enable C-style indentation
--- o.autoindent = true     -- (Commented out) Automatically indent new lines
 o.wrap = true             -- Wrap long lines
 o.textwidth = 300         -- Maximum width for text wrapping (set very high)
-o.tabstop = 2             -- Number of spaces for a tab character
-o.shiftwidth = 0          -- Use the value of 'tabstop' for auto-indentation
+o.tabstop = 4             -- Number of spaces for a tab character
+o.smarttab = true         -- Insert spaces when pressing <Tab> in insert mode
+o.autoindent = true       -- Automatically indent new lines
+o.shiftwidth = 4          -- Use the value of 'tabstop' for auto-indentation
 o.softtabstop = -1        -- If negative, use the value of 'shiftwidth' for <Tab>/<BS>
 o.list = true             -- Show invisible characters
-o.listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂' -- Define how invisible characters are displayed
 o.scrolloff = 8           -- Minimum number of lines to keep above and below the cursor
 o.sidescrolloff = 8       -- Minimum number of columns to keep left and right of the cursor
 o.timeoutlen = 500        -- Time (ms) to wait for a mapped sequence to complete
@@ -51,7 +42,7 @@ o.clipboard = 'unnamedplus'
 o.ignorecase = true
 o.smartcase = true
 
--- Undo and backup options
+-- Undo and backup
 o.backup = false          -- Do not create backup files
 o.writebackup = false     -- Do not keep a backup while overwriting a file
 o.undofile = false        -- Do not save undo history to a file
--- .config/nvim/lua/plugins_config.lua
+++ .config/nvim/lua/plugins_config.lua
@@ -9,51 +9,8 @@ vim.cmd [[
 colorscheme gruvbox-material
 ]]
 
--- Fuzzy finder
-require('telescope').setup({
-  defaults = {
-    -- configure to use ripgrep
-    vimgrep_arguments = {
-      "rg",
-      "--follow",        -- Follow symbolic links
-      "--hidden",        -- Search for hidden files
-      "--no-heading",    -- Don't group matches by each file
-      "--with-filename", -- Print the file path with the matched lines
-      "--line-number",   -- Show line numbers
-      "--column",        -- Show column numbers
-      "--smart-case",    -- Smart case search
-      "--word-regexp",   -- Full word
-
-      -- Exclude some patterns from search
-      "--glob=!**/.git/*",
-      "--glob=!**/.idea/*",
-      "--glob=!**/.vscode/*",
-      "--glob=!**/build/*",
-      "--glob=!**/dist/*",
-      "--glob=!**/yarn.lock",
-      "--glob=!**/package-lock.json",
-    },
-  },
-  pickers = {
-    find_files = {
-      hidden = true,
-      -- when not included or specified in .gitignore
-      find_command = {
-        "rg",
-        "--files",
-        "--hidden",
-        "--glob=!**/.git/*",
-        "--glob=!**/.idea/*",
-        "--glob=!**/.vscode/*",
-        "--glob=!**/build/*",
-        "--glob=!**/dist/*",
-        "--glob=!**/yarn.lock",
-        "--glob=!**/package-lock.json",
-      },
-    },
-  },
-})
-require('telescope').load_extension('fzf')
+-- Fuzzy finder (mini.pick)
+require('mini.pick').setup({})
 
 -- Git modifications sign
 require('gitsigns').setup()
@@ -63,3 +20,20 @@ require('lualine').setup()
 
 -- Markdown Preview
 g.mkdp_auto_start = 0
+
+-- Completion (blink.cmp)
+require('blink.cmp').setup({
+  keymap = { preset = 'default' },
+
+  completion = {
+    documentation = { auto_show = true },
+  },
+
+  sources = {
+    default = { 'lsp', 'path', 'snippets', 'buffer' },
+  },
+
+  fuzzy = {
+    implementation = "lua",
+  },
+})
