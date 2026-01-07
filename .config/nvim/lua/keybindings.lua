867790229a472d2c5f0702f15e255a46813f0a25
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2026-01-08 01:23:23 +0530

    add more keybinds

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -8,11 +8,20 @@ map('n', '<leader>qq', ':bw!<CR>')
 -- :noh on <leader>h
 map('n', '<leader>h', ':noh<CR>')
 
--- Telescope
+-- Telescope stuff
+-- files & buffers
 map('n', '<leader>f', ':Telescope find_files<CR>')
 map('n', '<leader>/', ':Telescope live_grep<CR>')
 map('n', '<leader>b', ':Telescope buffers<CR>')
+map('n', '<leader>j', ':Telescope jumplist<CR>')
+
+-- git
+map('n', '<leader>gb', ':Telescope git_branches<CR>')
+map('n', '<leader>gs', ':Telescope git_status<CR>')
+
+-- lsp
 map('n', '<leader>ls', ':Telescope lsp_document_symbols<CR>')
+map('n', '<leader>lw', ':Telescope lsp_dynamic_workspace_symbols<CR>')
 
 -- File Explorer
 map('n', '<leader>e', ':Yazi<CR>')
