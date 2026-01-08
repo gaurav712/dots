03221a6c2b0eae80baa9630ebb55cc8cc06a0cc9
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2026-01-09 00:43:38 +0530

    drop avante

--- .config/nvim/lua/plugins_config.lua
+++ .config/nvim/lua/plugins_config.lua
@@ -38,17 +38,3 @@ require('blink.cmp').setup({
   },
 })
 
-require('avante').setup({
-  provider = "claude",
-  providers = {
-    claude = {
-      endpoint = "https://api.anthropic.com",
-      model = "claude-sonnet-4-20250514",
-      timeout = 30000,
-      extra_request_body = {
-        temperature = 0.75,
-        max_tokens = 20480,
-      },
-    },
-  }
-})
