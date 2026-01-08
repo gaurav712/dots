60768fb02a87aeedcca2651ff449f537b7133fd6
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2026-01-09 01:17:51 +0530

    back to telescope; simplify plugins building logic

--- .config/nvim/lua/plugins_config.lua
+++ .config/nvim/lua/plugins_config.lua
@@ -9,8 +9,51 @@ vim.cmd [[
 colorscheme gruvbox-material
 ]]
 
--- Fuzzy finder (mini.pick)
-require('mini.pick').setup({})
+-- Fuzzy finder
+require('telescope').setup({
+  defaults = {
+    -- configure to use ripgrep
+    vimgrep_arguments = {
+      "rg",
+      "--follow",        -- Follow symbolic links
+      "--hidden",        -- Search for hidden files
+      "--no-heading",    -- Don't group matches by each file
+      "--with-filename", -- Print the file path with the matched lines
+      "--line-number",   -- Show line numbers
+      "--column",        -- Show column numbers
+      "--smart-case",    -- Smart case search
+      "--word-regexp",   -- Full word
+      -- Exclude some patterns from search
+      "--glob=!**/.git/*",
+      "--glob=!**/.idea/*",
+      "--glob=!**/.vscode/*",
+      "--glob=!**/build/*",
+      "--glob=!**/dist/*",
+      "--glob=!**/yarn.lock",
+      "--glob=!**/package-lock.json",
+    },
+  },
+  pickers = {
+    find_files = {
+      hidden = true,
+      -- when not included or specified in .gitignore
+      find_command = {
+        "rg",
+        "--files",
+        "--hidden",
+        "--glob=!**/.git/*",
+        "--glob=!**/.idea/*",
+        "--glob=!**/.vscode/*",
+        "--glob=!**/build/*",
+        "--glob=!**/dist/*",
+        "--glob=!**/yarn.lock",
+        "--glob=!**/package-lock.json",
+      },
+    },
+  },
+})
+
+require('telescope').load_extension('fzf')
 
 -- Git modifications sign
 require('gitsigns').setup()
@@ -30,7 +73,7 @@ require('blink.cmp').setup({
   },
 
   sources = {
-    default = { 'lsp', 'path', 'snippets', 'buffer' },
+    default = { 'lsp', 'path', 'buffer' },
   },
 
   fuzzy = {
